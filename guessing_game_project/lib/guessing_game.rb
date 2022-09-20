377ee7de77ecdc0f24bdfd5cfe7ca18c82db45d1
korycfitz <korycfitz@gmail.com>
2022-09-20 10:14:01 -0400

    check_num completed with specs passed and ask_user defined

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -2,27 +2,40 @@ class GuessingGame
     def initialize(min, max)   
         @min = min
         @max = max
-        @secret_num = rand  #random num b/w min and max inclusive
+        @secret_num = rand(min...max+1)  #random num b/w min and max inclusive
         @num_attempts = 0
         @game_over = false  
     end
 
     def num_attempts
-        @num_attempts = 0
+        @num_attempts
     end
 
     def game_over?
-        @game_over = false
+        @game_over
     end
 
     def check_num(num)
-        if num == secret_num
-            @game_over == true
+        @num_attempts +=1
+        @game_over
+        if num == @secret_num
+            @game_over = true
+            print 'you win'
+        end
+        if num > @secret_num
+            print 'too big'
+        else
+            print 'too small'
         end
     end
 
+    def ask_user
+    end
 
 
+            
 
 
 end
+
+
