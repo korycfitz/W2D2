f8379222687805dd157c94b86c22d0dc7d024473
korycfitz <korycfitz@gmail.com>
2022-09-20 10:00:33 -0400

    initialized @instance_variables and defined getters for num_attempts and game_over

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,28 @@
 class GuessingGame
+    def initialize(min, max)   
+        @min = min
+        @max = max
+        @secret_num = rand  #random num b/w min and max inclusive
+        @num_attempts = 0
+        @game_over = false  
+    end
+
+    def num_attempts
+        @num_attempts = 0
+    end
+
+    def game_over?
+        @game_over = false
+    end
+
+    def check_num(num)
+        if num == secret_num
+            @game_over == true
+        end
+    end
+
+
+
+
 
 end
