db69a890066cef9c3eb7b20918f002d9de264554
korycfitz <korycfitz@gmail.com>
2022-09-20 11:58:42 -0400

    getters

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,10 +1,11 @@
 class Hangman
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
     def self.random_word
       DICTIONARY.sample
       # DICTIONARY[rand(DICTIONARY.length - 1)] #also works but need to use Array#sample
     end
-
+  
     def initialize
       @secret_word = Hangman.random_word #can't use self b/c self refers to instance
       @guess_word = Array.new(@secret_word.length, "_")
@@ -12,10 +13,11 @@ class Hangman
       @remaining_incorrect_guesses = 5
     end
 
-    
+    def already_attempted?(char)
+    end
     
 
-
+  
 
 
 
